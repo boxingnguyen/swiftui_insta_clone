684de545e473688f87a662e2b76dd614686d9331
rs-anhnq2 <anhnq2@runsystem.net>
2021-12-08 15:02:46 +0700

    Initial Commit

---
+++ AnhnqInstaClone/AnhnqInstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnhnqInstaCloneApp.swift
+//  AnhnqInstaClone
+//
+//  Created by anhnq2 on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnhnqInstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
